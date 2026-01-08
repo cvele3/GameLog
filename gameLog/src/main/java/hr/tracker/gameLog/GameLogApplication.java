5202b4d17894cb197ebcae3b9b3bb6c5f220503d
cvele3 <cvetkojakov@gmail.com>
2026-01-08 14:20:24 +0100

    Initialization of sprig boot project

---
+++ gameLog/src/main/java/hr/tracker/gameLog/GameLogApplication.java
@@ -0,0 +1,13 @@
+package hr.tracker.gameLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLogApplication.class, args);
+	}
+
+}
